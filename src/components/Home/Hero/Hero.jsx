7647f13c36a23c478889ede26a81e2d2b17a3e3d
AmDeeJhay <amdeejhay@gmail.com>
2024-11-12 15:56:20 +0100

    i made it responsive

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,12 +1,25 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/scss/pagination';
+import 'swiper/swiper-bundle.min.css';
 import './Hero.scss';
+
 import analysis from '../../../assets/images/analysis.png';
 import Mask from '../../../assets/images/Mask.png';
 import charts from '../../../assets/images/charts.png';
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 1024);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="hero">
       <div className="hero-text">
@@ -17,11 +30,26 @@ const Hero = () => {
           <button>Learn More</button>
         </div>
       </div>
-      
+
       <div className="hero-image">
-        <img src={analysis} alt="analysis" />
-        <img src={Mask} alt="Mask" />
-        <img src={charts} alt="charts" />
+        {isMobile ? (
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            autoplay={{ delay: 2000 }}
+            pagination={{ clickable: true }}
+            loop={true}
+          >
+            <SwiperSlide><img src={analysis} alt="analysis" /></SwiperSlide>
+            <SwiperSlide><img src={Mask} alt="Mask" /></SwiperSlide>
+            <SwiperSlide><img src={charts} alt="charts" /></SwiperSlide>
+          </Swiper>
+        ) : (
+          <>
+            <img src={analysis} alt="analysis" />
+            <img src={Mask} alt="Mask" />
+            <img src={charts} alt="charts" />
+          </>
+        )}
       </div>
     </section>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-            <div className="logo">Kside</div>
-            <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    );
+      <div className="logo">Kside</div>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
