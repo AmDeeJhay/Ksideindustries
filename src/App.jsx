d4f4b7c3409539bfc682064a83effd9446b71ed1
AmDeeJhay <amdeejhay@gmail.com>
2024-11-18 12:03:47 +0100

    i made changes on some animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Home/Hero/Hero'
 import KsideDriven from './components/Home/KsideDriven/KsideDriven'
@@ -7,7 +7,7 @@ import FeaturedProject from './components/Home/FeaturedProject/FeaturedProject'
 import Form from './components/Home/Form/Form'
 import Map from './components/Home/Map/Map'
 import Footer from './components/Footer/Footer'
-import NotFoundPage from './components/Home/404Page/404Page'
+// import NotFoundPage from './components/Home/404Page/404Page'
 
 function App() {
   return (
@@ -23,14 +23,14 @@ function App() {
   );
 }
 
-<Router>
+{/* <Router>
   <Routes>
     <Route path="/" element={<FeaturedProject />} />
     <Route path="/404-project1" element={<NotFoundPage />} />
     <Route path="/404-project2" element={<NotFoundPage />} />
     <Route path="/404-project3" element={<NotFoundPage />} />
   </Routes>
-</Router>;
+</Router>; */}
 
 export default App;
  
